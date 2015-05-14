9a245e9e4cdc067bb3d7ecb837bf4d0a2855abd4
bbr <erobober@gmail.com>
2015-05-14 16:42:49 +0300

    Change new line separators "\n" to PHP_EOL and add doc blocks.

--- app/src/Core/AppException.php
+++ app/src/Core/AppException.php
@@ -1,10 +1,19 @@
 <?php
 namespace UTI\Core;
 
+/**
+ * Class AppException
+ * @package UTI\Core
+ */
 class AppException extends \RuntimeException
 {
+    /**
+     * Returns exceptions message prepended with stack trace string
+     *
+     * @return string
+     */
     public function getError()
     {
-        return  parent::getTraceAsString() . "\n" . parent::getMessage() . "\n";
+        return parent::getTraceAsString() . PHP_EOL . parent::getMessage() . PHP_EOL;
     }
 }
